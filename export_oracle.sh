bfc034f98460fd74341c33c0d84b55a5b90d5901
Diego Cuasapaz <89151463+dcuasapaz@users.noreply.github.com>
2022-09-22 17:17:54 -0500

    Update export_oracle.sh
    Agregar comentarios y quitar el $ de las variables

--- export_oracle.sh
+++ export_oracle.sh
@@ -1,18 +1,22 @@
 # Ingresar los valores de los parametros
 
-$RUTA_LIB=
-$RUTA_LIB_ORACLE=
-$RUTA_PYTHON=
-$HIVEDB=
-$HIVETABLE=
-$VAL_FECHA_PROCESO=
-$JDBCURL= 
-$TDDB= 
-$TDHOST= 
-$TDPASS= 
-$TDUSER= 
-$TDTABLE= 
-$TDPORT= 
-$TDSERVICE=
+# Rutas de cada proceso
+RUTA_LIB=
+RUTA_LIB_ORACLE=
+RUTA_PYTHON=
+# Esquema y tabla en HIVE
+HIVEDB=
+HIVETABLE=
+# Fecha de proceso (yyyyMMdd)
+VAL_FECHA_PROCESO=
+# Parametro de Oracle
+JDBCURL= 
+TDDB= 
+TDHOST= 
+TDPASS= 
+TDUSER= 
+TDTABLE= 
+TDPORT= 
+TDSERVICE=
 
 /usr/hdp/current/spark2-client/bin/spark-submit --master local --jars $RUTA_LIB/$RUTA_LIB_ORACLE $RUTA_PYTHON/export_oracle.py --vTable=$HIVEDB.$HIVETABLE --vFechaProceso=$VAL_FECHA_PROCESO --vJdbcUrl=$JDBCURL --vTDDb=$TDDB --vTDHost=$TDHOST --vTDPass=$TDPASS --vTDUser=$TDUSER --vTDTable=$TDTABLE --vTDPort=$TDPORT --vTDService=$TDSERVICE
